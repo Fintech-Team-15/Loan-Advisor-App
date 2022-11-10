ec8802a5a62c930cb33b7f22983c18f126bec5b2
faruking <faruk.abdulganiyu@gmail.com>
2022-11-10 09:30:41 +0100

    Splash screen added

---
+++ app/src/main/java/com/fintech15/loanadvisor/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.fintech15.loanadvisor
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val handler = Handler()
+        handler.postDelayed( {
+            val i = Intent(applicationContext,MainActivity::class.java)
+            startActivity(i)
+            finish()
+        },5000)
+    }
+}
