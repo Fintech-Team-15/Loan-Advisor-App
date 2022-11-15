b5a2b1dd5c774e39afe3dfed0fa8df9129cd7c97
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2022-11-15 23:22:14 +0200

    feat(login): simple email and password input validation

--- app/src/main/java/com/fintech15/loanadvisor/LoginFragment.kt
+++ app/src/main/java/com/fintech15/loanadvisor/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.fintech15.loanadvisor
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,19 +9,29 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.fintech15.loanadvisor.databinding.FragmentLoginBinding
+import com.google.android.material.textfield.TextInputEditText
 
 /**
  * A simple [Fragment] subclass.
  */
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var emailTextInput: TextInputEditText
+    private lateinit var passwordTextInput: TextInputEditText
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        emailTextInput = binding.loginEmailTextInput
+        passwordTextInput = binding.loginPasswordTextInput
+
+        binding.loginButton.setOnClickListener {
+            validateEmail()
+            validatePassword()
+        }
 
         binding.forgotPassword.setOnClickListener {
             Toast.makeText(context, "Implement password reset UI", Toast.LENGTH_SHORT).show()
@@ -32,5 +43,29 @@ class LoginFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun validatePassword() {
+        val password = passwordTextInput.text.toString()
+
+        if (password.isEmpty()) {
+            passwordTextInput.error = "Password cannot be empty"
+        }
+
+        if (password.length < 6) {
+            passwordTextInput.error = "Password should be greater than 6 characters"
+        }
+
+    }
+
+    private fun validateEmail() {
+        val email = emailTextInput.text.toString()
+        if (email.isEmpty()) {
+            emailTextInput.error = "Email cannot be empty"
+        }
+
+        if (email.isNotEmpty() && !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailTextInput.error = "Please enter a valid email address"
+        }
     }
 }
