5f3c9d53363dd6fdaa9f5a5a5e7b7d683fd33e31
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2022-11-19 08:00:39 +0200

    refactor: remove unused imports

--- app/src/main/java/com/fintech15/loanadvisor/LoginFragment.kt
+++ app/src/main/java/com/fintech15/loanadvisor/LoginFragment.kt
@@ -1,7 +1,6 @@
 package com.fintech15.loanadvisor
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +8,6 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.fintech15.loanadvisor.databinding.FragmentLoginBinding
-import com.fintech15.loanadvisor.utils.Validator
 import com.fintech15.loanadvisor.utils.validateEmail
 import com.fintech15.loanadvisor.utils.validatePassword
 import com.google.android.material.textfield.TextInputLayout
