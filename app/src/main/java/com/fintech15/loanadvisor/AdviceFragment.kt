868314bca4f1a7d6bf3b02b2359aba90d9c4efc1
kitsoNamane <kitsogideonnamane@gmail.com>
2022-11-23 15:17:18 +0200

    feat: add advice screen/page

--- app/src/main/java/com/fintech15/loanadvisor/AdviceFragment.kt
+++ app/src/main/java/com/fintech15/loanadvisor/AdviceFragment.kt
@@ -5,21 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.fintech15.loanadvisor.databinding.FragmentHomeBinding
+import com.fintech15.loanadvisor.databinding.FragmentAdviceBinding
 
 
 /**
  * A simple [Fragment] subclass.
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+class AdviceFragment : Fragment() {
+    private lateinit var binding: FragmentAdviceBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentAdviceBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
--- app/src/main/java/com/fintech15/loanadvisor/LoanCalculatorFragment.kt
+++ app/src/main/java/com/fintech15/loanadvisor/LoanCalculatorFragment.kt
@@ -1,10 +1,11 @@
 package com.fintech15.loanadvisor
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.fintech15.loanadvisor.databinding.FragmentLoanCalculatorBinding
 
 
@@ -22,6 +23,11 @@ class LoanCalculatorFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentLoanCalculatorBinding.inflate(inflater, container, false)
+        binding.getAdviceButton.setOnClickListener {
+            val action =
+                LoanCalculatorFragmentDirections.actionLoanCalculatorFragmentToAdviceFragment()
+            view?.findNavController()?.navigate(action)
+        }
         return binding.root
     }
 }
