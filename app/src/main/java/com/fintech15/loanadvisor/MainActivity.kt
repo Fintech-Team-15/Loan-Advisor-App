43d4f1918570b31f74e1759726d9cc4663227d47
kitsoNamane <kitsogideonnamane@gmail.com>
2022-11-23 17:06:52 +0200

    feat: setup destination aware toolbar and bottom navigation

--- app/src/main/java/com/fintech15/loanadvisor/MainActivity.kt
+++ app/src/main/java/com/fintech15/loanadvisor/MainActivity.kt
@@ -2,9 +2,12 @@ package com.fintech15.loanadvisor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.fintech15.loanadvisor.databinding.ActivityMainBinding
 
@@ -18,9 +21,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.materialToolbar)
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
         val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _: NavController, navDestination: NavDestination, _: Bundle? ->
+            when(navDestination.id) {
+                R.id.advice_fragment -> binding.bottomNavigation.isVisible = false
+                R.id.login_fragment, R.id.registration_fragment -> {
+                    binding.bottomNavigation.isVisible = false
+                    binding.materialToolbar.isVisible = false
+                }
+                else -> {
+                    binding.bottomNavigation.isVisible = true
+                    binding.materialToolbar.isVisible = true
+                }
+            }
+        }
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.materialToolbar.setupWithNavController(navController, appBarConfiguration)
 
         binding.bottomNavigation.setupWithNavController(navController)
         binding.navView.setupWithNavController(navController)
