23eba5ae111c79d2ed7d8ff3d13e00db12ac482a
kitsoNamane <kitsogideonnamane@gmail.com>
2022-11-24 16:41:19 +0200

    feat(translations): add tswana translations

--- app/src/main/java/com/fintech15/loanadvisor/data/DefaultAuthenticationRepository.kt
+++ app/src/main/java/com/fintech15/loanadvisor/data/DefaultAuthenticationRepository.kt
@@ -1,11 +1,24 @@
 package com.fintech15.loanadvisor.data
 
-class DefaultAuthenticationRepository : AuthenticationRepository {
+import com.google.firebase.auth.FirebaseAuth
+
+class DefaultAuthenticationRepository(val firebaseAuthSource: FirebaseAuth) : AuthenticationRepository {
+
     override fun loginWithEmailAndPassword(email: String, password: String): Result<User> {
-        return Result.Success(User(email=email, isLoggedIn = true, userName = email))
+        return try {
+            val auth = firebaseAuthSource.signInWithEmailAndPassword(email, password)
+            Result.Success(User(email=auth.result?.user?.email!!, isLoggedIn = true, userName = auth.result.additionalUserInfo?.username!!))
+        } catch (e: Exception) {
+           Result.Error(e)
+        }
     }
 
     override fun registerWithEmailAndPassword(email: String, password: String): Result<User> {
-        return Result.Success(User(email=email, isLoggedIn = true, userName = email))
+        return try {
+            val auth = firebaseAuthSource.signInWithEmailAndPassword(email, password)
+            Result.Success(User(email=auth.result?.user?.email!!, isLoggedIn = true, userName = auth.result.additionalUserInfo?.username!!))
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
     }
 }
